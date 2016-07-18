427edec4eee87dcbeb1ac62b97eb4d4689b53fc3
oneputin <git@iimaps.de>
2016-07-18 16:05:02 +0200

    extend firequery with item-select

---
+++ src/app-config.js
@@ -0,0 +1,12 @@
+var fbApp;
+var fbConfig = {
+    "wolke7": {  
+      "apiKey":        "AIzaSyC32ZX3rKvRFJTGZ7vVxxQX-WJCDPC01Dk",
+      "authDomain":    "wolke7-fd941.firebaseapp.com",
+      "databaseURL":   "https://wolke7-fd941.firebaseio.com",
+      "storageBucket": "wolke7-fd941.appspot.com"
+    },
+    "wolke9" :{
+    }
+};
+
